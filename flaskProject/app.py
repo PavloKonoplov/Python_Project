2f7c8af7de650de585c6fc9bd41c0e4c9aa08f41
PavloKonoplov <38275433+PavloKonoplov@users.noreply.github.com>
2020-11-04 11:21:49 +0200

    Add server

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,16 +1,17 @@
+#http://127.0.0.1:5000/api/v1/hello-world-10
 from flask import Flask
-from gevent.pywsgi import WSGIServer
-from yourapplication import app
+from wsgiref.simple_server import make_server
 
-http_server = WSGIServer(('', 5000), app)
-http_server.serve_forever()
 app = Flask(__name__)
 
 
 @app.route('/api/v1/hello-world-10')
 def hello_world():
-    return 'Hello World! {10}'
+    return 'Hello World! 10'
 
 
 if __name__ == '__main__':
     app.run()
+
+server = make_server('', 5000, app)
+server.serve_forever()
